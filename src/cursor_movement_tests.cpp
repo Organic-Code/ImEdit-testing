bf02ed701fbc2d3d9fb325698c792251ab1a1ddb
Lucas Lazare <lazarelucas@yahoo.fr>
2024-07-09 09:01:46 +0200

    Adding utf8 arrow key navigation tests

--- src/cursor_movement_tests.cpp
+++ src/cursor_movement_tests.cpp
@@ -98,7 +98,69 @@ void add_cursor_movement_tests(ImGuiTestEngine* engine) {
         ctx->KeyPress(ImGuiKey_Home);
         IM_CHECK(editor.has_cursor({3, 0, 0}));
     };
-    // TODO same for UTF-8
+
+    test = IM_REGISTER_TEST(engine, scm_name, "arrow key navigation within UTF-8 text");
+    test->GuiFunc = [&](ImGuiTestContext*) {
+        if (ImGui::Begin(window_name)) {
+            editor.render();
+        }
+        ImGui::End();
+    };
+    test->TestFunc = [&](ImGuiTestContext* ctx) {
+        // taking focus
+        ctx->WindowResize(window_name, ImVec2{500.f, 250.f});
+        ctx->SetRef(window_name);
+        ctx->MouseMove(editor_name, ImGuiTestOpFlags_NoCheckHoveredId | ImGuiTestOpFlags_MoveToEdgeR | ImGuiTestOpFlags_MoveToEdgeU);
+        ctx->MouseClick(ImGuiMouseButton_Left);
+
+        // filling editor with starting data
+        set_data_utf8(editor);
+        editor.set_cursor({1, 6, 6});
+        IM_CHECK(editor.has_cursor({1, 6, 6}));
+
+        // basic moves
+        ctx->KeyPress(ImGuiKey_UpArrow);
+        IM_CHECK(editor.has_cursor({0, 6, 12}));
+        ctx->KeyPress(ImGuiKey_DownArrow);
+        IM_CHECK(editor.has_cursor({1, 6, 6}));
+        ctx->KeyPress(ImGuiKey_DownArrow);
+        IM_CHECK(editor.has_cursor({2, 8, 9}));
+        ctx->KeyPress(ImGuiKey_UpArrow);
+        IM_CHECK(editor.has_cursor({1, 6, 6}));
+        ctx->KeyPress(ImGuiKey_LeftArrow);
+        IM_CHECK(editor.has_cursor({1, 6, 3}));
+        ctx->KeyPress(ImGuiKey_RightArrow);
+        IM_CHECK(editor.has_cursor({1, 6, 6}));
+        ctx->KeyPress(ImGuiKey_RightArrow);
+        IM_CHECK(editor.has_cursor({1, 6, 9}));
+        ctx->KeyPress(ImGuiKey_LeftArrow);
+        IM_CHECK(editor.has_cursor({1, 6, 6}));
+
+        // next token and previous token
+        ctx->KeyPress(ImGuiKey_RightArrow | ImGuiMod_Ctrl);
+        IM_CHECK(editor.has_cursor({1, 7, 0}));
+        ctx->KeyPress(ImGuiKey_RightArrow | ImGuiMod_Ctrl);
+        IM_CHECK(editor.has_cursor({1, 8, 0}));
+
+        editor.set_cursor({1, 6, 6});
+        ctx->KeyPress(ImGuiKey_LeftArrow | ImGuiMod_Ctrl);
+        IM_CHECK(editor.has_cursor({1, 6, 0}));
+        ctx->KeyPress(ImGuiKey_LeftArrow | ImGuiMod_Ctrl);
+        IM_CHECK(editor.has_cursor({1, 5, 0}));
+
+        // next line and previous line using right and left arrows
+        editor.set_cursor({1, 0, 0});
+        ctx->KeyPress(ImGuiKey_LeftArrow);
+        IM_CHECK(editor.has_cursor({0, 8, 27}));
+        ctx->KeyPress(ImGuiKey_RightArrow);
+        IM_CHECK(editor.has_cursor({1, 0, 0}));
+
+        // end line / beg line
+        ctx->KeyPress(ImGuiKey_End);
+        IM_CHECK(editor.has_cursor({1, 12, 15}));
+        ctx->KeyPress(ImGuiKey_Home);
+        IM_CHECK(editor.has_cursor({1, 0, 0}));
+    };
 
     // TODO cursor pos when inputting text ? (maybe test that elsewhere)
     // TODO on cursor click
